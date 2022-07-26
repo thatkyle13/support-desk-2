ad6f6c6a4029d3b0d9d3faa66c6c483b99467f93
Kyle <kylewilfredc@gmail.com>
2022-07-27 03:29:56 +0800

    home login register UI

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,61 @@
-
+import {useState} from 'react'
+import {toast} from 'react-toastify'
+import {FaUser} from 'react-icons/fa'
 
 function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }) )
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if(password !== password2){
+      toast.error('Passwords do not match')
+    }
+  }
+
+  const {name, email, password, password2} = formData
   return (
-    <div>Register</div>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please Create an Account</p>
+      </section>
+
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input type="text" className="form-control" id='name' name='name' value={name} onChange={onChange} required placeholder='Enter your name'/>
+          </div>
+          <div className="form-group">
+            <input type="email" className="form-control" id='email' name='email' value={email} onChange={onChange} required placeholder='Enter your email'/>
+          </div>
+          <div className="form-group">
+            <input type="password" className="form-control" id='password' name='password' value={password} onChange={onChange} required placeholder='Enter password'/>
+          </div>
+          <div className="form-group">
+            <input type="password" className="form-control" id='password2' name='password2' value={password2} onChange={onChange} required placeholder='Confirm password'/>
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+
+        </form>
+      </section>
+    </>
   )
 }
 
