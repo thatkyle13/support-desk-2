92835b8fcd835bb57eb0e30eb9d1d98e546e1012
Kyle <kylewilfredc@gmail.com>
2022-07-28 06:18:42 +0800

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -14,13 +15,24 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Welcome to support desk api'})
-})
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+// Serve Frontend
+if(process.env.NODE_ENV === 'production'){
+    // set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+      })
+} else{
+    app.get('/', (req, res) => {
+        res.status(200).json({message: 'Welcome to support desk api'})
+    })
+}
 
 app.use(errorHandler)
 
